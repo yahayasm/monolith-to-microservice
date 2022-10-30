77c5e551e2320baa25d4662406c7f58cd64e796a
Yahayasm <yahayasulaiman83@yahoo.com>
2022-10-30 13:46:22 +0000

    Initial Commit

---
+++ udagram-api/set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=yahayasm
+export POSTGRES_PASSWORD=yahayasm
+export POSTGRES_HOST=monolith-microservice.cbznnve9ww92.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=monolith_microservice
+export AWS_BUCKET=monolith-microservice-dev
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=hello
+export URL=http://localhost:8100
